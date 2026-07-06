d6a9674f0bbd9006397b5ee234d16a0a0a4800ee
Koji Miyazaki <61367262+kouzi5816@users.noreply.github.com>
2026-07-06 15:44:41 +0900

    Add files via upload

--- 02_RAGApp/app.py
+++ 02_RAGApp/app.py
@@ -5,7 +5,7 @@ import tempfile
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
